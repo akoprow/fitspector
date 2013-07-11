e16b30d0a4826e5853f3059b141274904fd5ec9a
Adam Koprowski <koper@google.com>
2013-07-10 20:46:33 -0700

    Reading index.html from an external file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html').toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
