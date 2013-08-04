11b724f7040efce9f1e37e45c990dd4f40e2133c
Adam Koprowski <koper@google.com>
2013-08-03 19:25:26 -0700

    Added year labels.

--- js/vis-calendar.js
+++ js/vis-calendar.js
@@ -1,9 +1,23 @@
 var dataset = [
-  5, 10, 15, 20, 25
+  new Date(2012, 11, 3),
+  new Date(2013, 5, 7)
 ];
 
-d3.select("body").selectAll("p")
-  .data(dataset)
+var leftMargin = 30;
+var cellSize = 20;
+
+var width = cellSize * 53 + leftMargin;
+var height = cellSize * 7;
+
+var year = d3.select("#vis-calendar")
+  .selectAll("svg")
+    .data(d3.range(2005, 2013))
   .enter()
-    .append("p")
-    .text("New paragraph!");
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+year.append("text")
+  .attr("transform", "translate(" + leftMargin + "," + cellSize * 3.5 + ") rotate(-90)")
+  .style("text-anchor", "middle")
+  .text(function(d) { return d; });
