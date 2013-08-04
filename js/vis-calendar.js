466f0863d9d1f97dc87d1d5ae0f199c063302482
Adam Koprowski <koper@google.com>
2013-08-03 18:52:12 -0700

    Basic setup for Calendar visualization: including D3.

---
+++ js/vis-calendar.js
@@ -0,0 +1,9 @@
+var dataset = [
+  5, 10, 15, 20, 25
+];
+
+d3.select("body").selectAll("p")
+  .data(dataset)
+  .enter()
+    .append("p")
+    .text("New paragraph!");
--- web.js
+++ web.js
@@ -4,8 +4,21 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = fs.readFileSync('index.html').toString('utf-8');
-  response.send(index);
+  response.send(
+      fs.readFileSync('index.html')
+	  .toString('utf-8'));
+});
+
+app.get('/calendar', function(request, response) {
+  response.send(
+      fs.readFileSync('calendar.html')
+	  .toString('utf-8'));
+});
+
+app.get('/js/vis-calendar.js', function(request, response) {
+  response.send(
+      fs.readFileSync('js/vis-calendar.js')
+	  .toString('utf-8'));
 });
 
 var port = process.env.PORT || 8080;
