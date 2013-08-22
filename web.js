84d08383d0c11c2fce252cba937232ebfcdd044d
Adam Koprowski <koper@google.com>
2013-08-21 19:41:59 -0700

    Adding automatic redirection to https in production env.

--- web.js
+++ web.js
@@ -3,6 +3,17 @@ var server = express.createServer();
 
 server.use(express.compress());
 
+server.configure('production', function () {
+  server.use(function (req, res, next) {
+    var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
+    if (schema === 'https') {
+      next();
+    } else {
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
+});
+
 server.configure(function() {
   server.use(express.static(__dirname + '/public'));
 });
