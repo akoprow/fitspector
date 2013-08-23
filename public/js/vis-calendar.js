42c7f5386054c21e249a5bb63d56fffa7d334b9f
Adam Koprowski <koper@google.com>
2013-08-22 21:28:12 -0700

    Smoother transitions for filters.

--- public/js/vis-calendar.js
+++ public/js/vis-calendar.js
@@ -89,7 +89,7 @@ var prepareData = function(year, sport, unit, group) {
 	day: d.day,
 	value: total,
 	color: sports[e.exerciseType].color,
-	key: year + sport + unit + group + d.day
+	key: d.day
       };
     });
   });
