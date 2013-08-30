0fbf97d8389229de280d9645e830c7fb297db5a5
Adam Koprowski <koper@google.com>
2013-08-30 16:34:00 -0700

    Adding caching of images (for a year).

--- web.js
+++ web.js
@@ -15,6 +15,13 @@ server.configure('production', function () {
 });
 
 server.configure(function() {
+  server.use(function(req, res, next) {
+    if (req.url.indexOf('/img/') === 0) {
+      res.setHeader("Cache-Control", "max-age = 31556926"); // cache for a year
+    }
+    return next();
+  });
+
   server.use(express.static(__dirname + '/public'));
 });
 
