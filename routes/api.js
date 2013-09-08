dd5d61598e85b65d01f769755eee83af45b99f6b
Adam Koprowski <koper@google.com>
2013-09-08 14:52:54 -0700

    RunKeeper login API call returns user object back.

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,12 @@ var runKeeper = {
     user_info: {
       path: 'user',
       accept: 'application/vnd.com.runkeeper.User+json'
+    },
+    profile: function(user_info) {
+      return {
+        path: user_info.profile,
+        accept: 'application/vnd.com.runkeeper.Profile+json'
+      };
     }
   },
 
@@ -25,22 +31,32 @@ var runKeeper = {
         'Accept': config.accept
       }
     };
-    console.log('get params: %j', opts);
     request.get(opts, callback);
   }
 };
 
-var getUser = function(access_token) {
-  runKeeper.get(access_token, runKeeper.api.user_info, function(err, res, body) {
-    console.log('getUser: status code: ' + res.statusCode);
-    console.log('Body: %j', body);
-  });
+var getUser = function(access_token, callback) {
+  runKeeper.get(access_token, runKeeper.api.user_info, callback);
+};
+
+var getProfile = function(access_token, userData, callback) {
+  runKeeper.get(access_token, runKeeper.api.profile(userData), callback);
+};
+
+var mkUser = function(user_data, profile_data) {
+  return {
+    userID: 'RK' + user_data.userID,
+    isMale: profile_data.gender == 'M',
+    birthday: new Date(profile_data.birthday),
+    smallImg: profile_data.medium_picture
+  };
 };
 
 exports.loginRK = function(req, res) {
   var errorResponse = function() {
     // TODO(koper) ...
   };
+  console.log('RunKeeper login request');
 
   var params = {
     grant_type: "authorization_code",
@@ -53,13 +69,17 @@ exports.loginRK = function(req, res) {
   request.post({
     uri: runKeeper.api.access_token.uri,
     form: params
-  }, function(err, res, body) {
-    if (res.statusCode == 200) {
+  }, function(err, res1, body) {
+    if (res1.statusCode == 200) {
       try {
         var access_token = JSON.parse(body).access_token;
-        console.log('Access token: ' + access_token);
-        getUser(access_token);
-        // TODO(koper) ...
+        getUser(access_token, function(err, _, user_data) {
+          getProfile(access_token, user_data, function(err, _, profile_data) {
+            var userData = mkUser(user_data, profile_data);
+            console.log('Login successful: %j', userData);
+            res.send(userData);
+          });
+        });
       }
       catch (err) {
         errorResponse();
