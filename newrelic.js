80a592a9de4bfef71fef4cd0fc214c8d7c96b8b5
Adam Koprowski <koper@google.com>
2014-01-26 18:10:06 -0800

    publish: Adding newrelic.
    generated from commit 8c0e8d4dc2ef99ec06427b9a449b11ec67ccb157

---
+++ newrelic.js
@@ -0,0 +1,10 @@
+(function() {
+  exports.config = {
+    app_name: ['Fitspector'],
+    license_key: '/* @echo NEW_RELIC_LICENSE */',
+    logging: {
+      level: 'trace'
+    }
+  };
+
+}).call(this);
